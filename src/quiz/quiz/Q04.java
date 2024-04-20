02018b8983fe9c71562c9a386e0ba6d5c23e3572
chanceof <yong92co@gmail.com>
2024-04-20 18:07:42 +0900

    feat(Q04.java): Quiz04 남은 마나 구하기

---
+++ src/quiz/quiz/Q04.java
@@ -0,0 +1,33 @@
+package quiz.quiz;
+
+/**
+ * 현재 캐릭터의 총 MP은 1000입니다.
+ * 캐릭터의 스킬은 q, w, e, r이 있고
+ * 스킬 사용 시 소모되는 MP는
+ * - q : 10
+ * - w : 30
+ * - e : 50
+ * - r : 100
+ * q,w,e,r로 이루어진 문자열(스킬 구성)을 사용하고 남은 마나를 출력해주세요
+ * 만약 문자열로 구성된 모든 스킬을 사용하는데 필요한 MP가 부족하다면 "MP가 부족합니다"를 출력해주세요
+ *
+ * 예제1 input) "qwrrrqwer"
+ * 예제1 output) 남은 마나 : 470
+ *
+ * 예제2 input) "rrrqqweqwerrqwerqwerrqwerer"
+ * 예제2 output) MP가 부족합니다
+ */
+public class Q04 {
+    public static void main(String[] args) {
+        // 문자열 입력
+
+        // 남은 마나 가져오기
+
+        // 출력
+    }
+
+    private int getSmallestNumbersByAsc(String skills) {
+        //남은 마나 가져오기
+        return 0;
+    }
+}
