338f08871be1a0055ef6a805caa91ffc6b35c90f
chanceof <yong92co@gmail.com>
2024-04-21 02:32:40 +0900

    feat: Q005, Q006, Q007 추가

---
+++ src/quiz/quiz/Q06.java
@@ -0,0 +1,26 @@
+package quiz.quiz;
+
+/**
+ * 특정 문자열을 입력받고, 문자열에서 중복된 문자를 제거하고 남은 문자열을 출력하세요
+ *
+ * 예제1)
+ * a b c d e -> abcde
+ *
+ * 예제2)
+ * a a b b c c d e -> de
+ *
+ * 예제3)
+ * a b c d e f e d d d c a -> bf
+ *
+ * 예제4)
+ * a e a d d e e -> "없습니다"
+ */
+public class Q06 {
+    public static void main(String[] args) {
+        // 문자열 입력받기
+
+        // 중복된 문자 제거하기
+
+        // 출력하기
+    }
+}
