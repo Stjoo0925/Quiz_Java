a3825415add3c68c400c10df2f875d6a32baa1fb
Stjoo0925 <stjoo0925@gmail.com>
2024-04-21 16:50:51 +0900

    Fixes : Q04 add comment

--- src/quiz/quiz/Q04.java
+++ src/quiz/quiz/Q04.java
@@ -24,36 +24,35 @@ public class Q04 {
         // 문자열 입력
         // 남은 마나 가져오기
         // 출력
-        Scanner sc = new Scanner(System.in);
-        String skillOder = sc.nextLine();
+        Scanner sc = new Scanner(System.in);    // 스캐너 호출
+        String skillOder = sc.nextLine();   // skillOder에 스캐너로 문자열 입력
 
-        Q04 skill = new Q04();
-        int skills = skill.usedManacalculations(skillOder);
-
+        Q04 skill = new Q04();  //  메소드를 사용하기 위해 메모리에 올림
+        int skills = skill.usedManacalculations(skillOder); // usedManacalculations메소드의 리턴값을 skills에 받음
     }
 
     private int usedManacalculations(String skills) {
         //남은 마나 가져오기
-        String[] skillNum = skills.split(" ");
-        int fullMana = 1000;
-        int useMana = 0;
+        String[] skillNum = skills.split(" ");  // skillNum에 문자열형 skills를 space로 분리하여 저장
+        int fullMana = 1000;    // 마나 총량 초기화
+        int useMana = 0;    // 사용한 마나를 합산할 변수를 사용하기 위해 초기화
 
-        for (int i = 0; i < skillNum.length; i++) {
-            switch (skillNum[i]) {
+        for (int i = 0; i < skillNum.length; i++) { //  사용되는 마나를 계산하기 위한 반복문
+            switch (skillNum[i]) {  //skillNum배열에 나누어서 저장한 값을 case에 맞게 useMana에 합산
                 case "q" -> skillNum[i] = "10";
                 case "w" -> skillNum[i] = "30";
                 case "e" -> skillNum[i] = "50";
                 case "r" -> skillNum[i] = "100";
             }
             useMana += Integer.parseInt(skillNum[i]);
-        }
+        }   // 반복문 종료
 
-        if (useMana < fullMana) {
+        if (useMana < fullMana) {   // 만약 useMana가 fullMana보다 작다면 아래 출력문 출력
             System.out.println("남은 마나 : " + (fullMana - useMana));
-        } else {
+        } else {    // useMana가 더 크다면 아래 출력문 출력
             System.out.println("MP가 부족합니다.");
 
         }
-        return useMana;
+        return useMana; // useMana로 값을 리턴함
     }
 }
